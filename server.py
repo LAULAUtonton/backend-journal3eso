cb2321c3106f31ee9e2275820840edf6506d216f
LAULAUtonton <lestevezplasencia@gmail.com>
2026-02-09 16:41:11 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+import uuid
 
 app = FastAPI()
 
@@ -19,7 +22,23 @@ app.add_middleware(
 def root():
     return {"status": "Backend running"}
 
-# Endpoint que usará tu frontend
+# GET: obtener grupos (por ahora vacío)
 @app.get("/api/groups")
 def get_groups():
     return []
+
+# MODELO para crear grupo
+class GroupCreate(BaseModel):
+    group_name: str
+    members: List[str]
+    project_type: str
+
+# POST: crear grupo
+@app.post("/api/groups")
+def create_group(group: GroupCreate):
+    return {
+        "id": str(uuid.uuid4()),
+        "group_name": group.group_name,
+        "members": group.members,
+        "project_type": group.project_type,
+    }
